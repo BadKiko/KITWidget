abdf7c3eca2c9e6fd67b1e8194df686671e7493c
【﻿Ｋｉｋｏ】 <kirill.kif234@gmail.com>
2021-12-07 17:04:22 +0300

    Squashed commit of the following:
    commit bf3cf913a45ed360c8e826a34c5efc37d36b237a
    Author: Kiko <kirill.kif234@gmail.com>
    Date:   Tue Dec 7 16:33:58 2021 +0300
    
        Replacements Finished

---
+++ app/src/main/java/com/kikogames/kitwidget/RecieveBoot.kt
@@ -0,0 +1,12 @@
+package com.kikogames.kitwidget
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class RecieveBoot : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+    }
+}
